1f383af8a955a4bedfac173ad75a9c973259d7af
yves-antoine.gangner <yves-antoine.gangner@epita.fr>
2021-12-04 21:06:49 +0100

    préparation du programme final

--- OCR_MAIN/OCR.c
+++ OCR_MAIN/OCR.c
@@ -2,7 +2,24 @@
 // Created by yvesantoine on 04/12/2021.
 //
 
-void solve_image(const char path)
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <libgen.h>
+#include <string.h>
+
+void solve_image(const char * path)
 {
+    char real_path[PATH_MAX];
+    realpath(path, real_path);
+    printf("%s\n", real_path);
+    char * dir_name = dirname(real_path);
+    printf("%s\n", dir_name);
+
+    char new_path[PATH_MAX];
+    strcpy(new_path, dir_name);
+    strcat(new_path, "/numbers");
+    printf("%s\n", new_path);
+
     return;
 }
---
+++ OCR_MAIN/main.c
@@ -0,0 +1,6 @@
+#include "OCR.h"
+
+int main(){
+    solve_image("../Image/images/before.bmp");
+    return 0;
+}
