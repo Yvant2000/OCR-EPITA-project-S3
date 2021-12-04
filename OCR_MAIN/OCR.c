6352e67afd10c0ab7d277b074750b215127228c5
yves-antoine.gangner <yves-antoine.gangner@epita.fr>
2021-12-04 20:16:03 +0100

    created new folder

---
+++ OCR_MAIN/OCR.c
@@ -0,0 +1,8 @@
+//
+// Created by yvesantoine on 04/12/2021.
+//
+
+void solve_image(const char path)
+{
+    return;
+}
---
+++ OCR_MAIN/OCR.h
@@ -0,0 +1,10 @@
+//
+// Created by yvesantoine on 04/12/2021.
+//
+
+#ifndef OCR_EPITA_PROJECT_S3_OCR_H
+#define OCR_EPITA_PROJECT_S3_OCR_H
+
+void solve_image(const char path);
+
+#endif //OCR_EPITA_PROJECT_S3_OCR_H
